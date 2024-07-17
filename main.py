873c57bce80c848255d25630ffe8dccd7470be38
Shubham Aggarwal <saggarwal@corp.etz.com>
2024-07-17 11:09:04 -0500

    Added function to give acces to

--- main.py
+++ main.py
@@ -12,15 +12,22 @@ class DocGroupAccess(tk.Tk):
         self.user_pk = None
 
         self.user_data_dict = self.database_conn.get_user_data()
-        self.user_names = list(self.user_data_dict.values())
-        self.user_data_list = [(v, k) for k, v in self.user_data_dict.items()]
+        # self.user_names = list(" ".join(self.user_data_dict.values()[0], self.user_data_dict.values()[1]))
+        self.user_data_list = [(value[0], value[1]) for value in self.user_data_dict.values()]
+        # [(v, k) for k, v in self.user_data_dict.items()]
         self.user_display_list = [
-            f"{name} (UserPK: {pk})" for name, pk in self.user_data_list
+            f"{firstname} {lastname}" for firstname, lastname in self.user_data_list
         ]
         self.current_user_display_list = (
             self.user_display_list.copy()
         )  # Store current display list
 
+        self.department_dict = self.database_conn.get_department()
+        self.department_names = list(self.department_dict.values())
+        self.department_data_list = [(v, k) for k, v in self.department_dict.items()]
+        self.department_display_list = [
+            f"{name}" for name, pk in self.department_data_list
+            ]
         self.document_group_dict = self.database_conn.get_document_groups()
         self.document_groups = list(self.document_group_dict.values())
         self.make_combobox()
@@ -78,15 +85,32 @@ class DocGroupAccess(tk.Tk):
             self.multi_user_listbox.insert(tk.END, user)
         self.multi_user_listbox.grid(row=7, column=2)
 
+        remove_non_active_users_access_button = tk.Button(self, text="Remove Non Active Access", command=self.database_conn.remove_access_non_active_user)
+        remove_non_active_users_access_button.grid(row=9, column=1)
+
+        tk.Label(self, text="Select Department: ").grid(row=10,column=0)
+        self.department_combobox = ttk.Combobox(self, values=self.department_display_list, state="normal")
+        self.department_combobox.grid(row=10, column=1)
+
+        give_dept_access_button = tk.Button(self, text="Give Dept Access", command= lambda: self.give_or_remove_department_access("give"))
+        give_dept_access_button.grid(row=11, column=0)
+
+        remove_dept_access_button = tk.Button(self, text="Remove Dept Access", command=lambda: self.give_or_remove_department_access("remove"))
+        remove_dept_access_button.grid(row=11, column=1)
+
+        maintain_dept_access_button = tk.Button(self, text="Maintain Dept Access", command=self.database_conn.maintain_department_access)
+        maintain_dept_access_button.grid(row=11, column=2)
+
     def filter_by_enabled_user(self):
         if self.enabled_user_var.get():
             user_data_dict = self.database_conn.get_user_data(enabled=True)
         else:
             user_data_dict = self.database_conn.get_user_data()
 
-        user_data_list = [(v, k) for k, v in user_data_dict.items()]
+        user_data_list = [(value[0], value[1]) for value in user_data_dict.values()]
+        # [(v, k) for k, v in user_data_dict.items()]
         self.user_display_list = [
-            f"{name} (UserPK: {pk})" for name, pk in user_data_list
+            f"{name} {lastname}" for name, lastname in user_data_list
         ]
         self.current_user_display_list = self.user_display_list.copy()
 
@@ -108,7 +132,15 @@ class DocGroupAccess(tk.Tk):
 
     def update_selected_user_info(self, event):
         selected_user_display = self.user_combobox.get()
-        self.user_pk = selected_user_display.split(" (UserPK: ")[1][:-1]
+        user_first_name = selected_user_display.split(" ")[0]
+        user_last_name = selected_user_display.split(" ")[1]
+        # for first_name in self.user_data_dict.values():
+        #     if first_name[0] == user_first_name:
+        #         self.selected_user_pk = self.user_data_dict[first_name]
+        for key, value in self.user_data_dict.items():
+            if value[0] == user_first_name and value[1] == user_last_name:
+                self.user_pk = key
+        # self.user_pk = selected_user_display.split(" (UserPK: ")[1][:-1]
         self.doc_user_group_dict = self.database_conn.get_accesed_document_group(
             self.user_pk, self.document_group_dict
         )
@@ -138,7 +170,15 @@ class DocGroupAccess(tk.Tk):
             for ind in selected_user_indices:
                 selected_user = self.multi_user_listbox.get(ind)
                 # selected_user_pk = None
-                selected_user_pk = selected_user.split(" (UserPK: ")[1][:-1]
+                user_first_name = selected_user.split(" ")[0]
+                user_last_name = selected_user.split(" ")[1]
+                # for first_name in self.user_data_dict.values():
+                #     if first_name[0] == user_first_name:
+                #         self.selected_user_pk = self.user_data_dict[first_name]
+                for key, value in self.user_data_dict.items():
+                    if value[0] == user_first_name and value[1] == user_last_name:
+                        selected_user_pk = key
+                # selected_user_pk = selected_user.split(" (UserPK: ")[1][:-1]
 
                 if selected_doc_group_pk and selected_user_pk:
                     self.database_conn.add_document_group_user(
@@ -182,6 +222,34 @@ class DocGroupAccess(tk.Tk):
         for pk, group in self.doc_user_group_dict.items():
             self.database_conn.delete_document_group_user(pk)
         self.update_selected_user_info(None)
+    
+    def give_or_remove_department_access(self, access):
+        selected_indices = self.document_group_listbox.curselection()
+        for index in selected_indices:
+            selected_doc_group = self.document_group_listbox.get(index)
+            selected_doc_group_pk = None
+            for key, value in self.document_group_dict.items():
+                if value == selected_doc_group:
+                    selected_doc_group_pk = key
+                    break
+            selected_department = self.department_combobox.get()
+            for key, value in self.department_dict.items():
+                if value == selected_department:
+                    selected_department_pk = key
+            # selected_dpartment_pk = selected_department.split(" (DepartmentPK: ")[1][:-1]
+            if access == "give":
+                self.database_conn.department_access("Give", selected_department_pk, selected_doc_group_pk)
+            elif access == "remove":
+                self.database_conn.department_access("Remove", selected_department_pk, selected_doc_group_pk)
+        
+        if self.user_pk:
+            self.update_selected_user_info(None)
+        self.document_group_listbox.selection_clear(0, tk.END)
+        self.multi_user_listbox.selection_clear(0, tk.END)
+
+    
+    def maintain_all_department_access(self):
+        pass
 
 
 if __name__ == "__main__":
--- src/mie_trak.py
+++ src/mie_trak.py
@@ -6,8 +6,9 @@ class MieTrak:
         self.user_table = TableManger("[User]")
         self.document_group_table = TableManger("DocumentGroup")
         self.document_group_users_table = TableManger("DocumentGroupUsers")
+        self.department_table = TableManger("Department")
 
-    def get_user_data(self, enabled=False):
+    def get_user_data(self, enabled=False, not_active=False, departmentfk=None):
         """Returns all the user data in the form of a dict with UserPK as Key and FirstName as Value
 
         Returns:
@@ -15,14 +16,18 @@ class MieTrak:
         """
         user_dict = {}
         if enabled:
-            user = self.user_table.get("UserPK", "FirstName", Enabled=1)
+            user = self.user_table.get("UserPK", "FirstName", "LastName", Enabled=1)
+        elif not_active:
+            user = self.user_table.get("UserPK", "FirstName", "LastName",Enabled=0)
+        elif departmentfk:
+            user = self.user_table.get("UserPK", "FirstName", "LastName",DepartmentFK=departmentfk, Enabled=1)
         else:
-            user = self.user_table.get("UserPK", "FirstName")
+            user = self.user_table.get("UserPK", "FirstName", "LastName")
 
         if user:
             for x in user:
                 if x:
-                    user_dict[x[0]] = x[1]
+                    user_dict[x[0]] = [x[1],x[2]]
         return user_dict
 
     def get_document_groups(self):
@@ -57,6 +62,63 @@ class MieTrak:
 
     def add_document_group_user(self, doc_group_pk, user_fk):
         """Adds a new entry to the DocumentGroupUsers table"""
-        info_dict = {"DocumentGroupFK": doc_group_pk, "UserFK": user_fk}
-        pk = self.document_group_users_table.insert(info_dict)
+        pk = self.document_group_users_table.get("DocumentGroupUsersPK", UserFK=user_fk, DocumentGroupFK=doc_group_pk)
+        if not pk:
+            info_dict = {"DocumentGroupFK": doc_group_pk, "UserFK": user_fk}
+            pk = self.document_group_users_table.insert(info_dict)
         return pk
+    
+    def remove_access_non_active_user(self):
+        """Removes access to all non active users"""
+        non_active_user_dict = self.get_user_data(not_active=True)
+        doc_group_dict = self.get_document_groups()
+        for user_pk in non_active_user_dict.keys():
+            doc_user_group_dict = self.get_accesed_document_group(user_pk, doc_group_dict)
+            if doc_user_group_dict:
+                for doc_group_pk in doc_user_group_dict.keys():
+                    self.delete_document_group_user(doc_group_pk) 
+
+    def get_department(self):
+        """Returns all the Department data in the form of a dict with DepartmentPK as Key and Name as value"""
+        department_dict = {}
+        departments = self.department_table.get("DepartmentPK", "Name")
+        if departments:
+            for x in departments:
+                if x:
+                    department_dict[x[0]] = x[1]
+        return department_dict
+    
+    def department_access(self, access, departmentfk, document_group_fk):
+        user = self.get_user_data(departmentfk=departmentfk)
+        print(user)
+        if access=="Give":
+            for user_pk in user.keys():
+                self.add_document_group_user(document_group_fk, user_pk)
+        elif access=="Remove":
+            for user_pk1 in user.keys():
+                document_group_users_pk = self.document_group_users_table.get("DocumentGroupUsersPK", DocumentGroupFK=document_group_fk, UserFK=user_pk1)
+                if document_group_users_pk:
+                    for pk in document_group_users_pk:
+                        self.delete_document_group_user(pk[0])
+
+    def maintain_department_access(self):
+        """Maintains the access of the users to the documents based on the department they belong to"""
+        department_doc_groups = {
+
+        }
+        document_group_dict = self.get_document_groups()
+        department_dict = self.get_department()
+        for departmentfk in department_dict.keys():
+
+            user = self.get_user_data(departmentfk=departmentfk)
+            for user_pk in user.keys():
+                doc_user_group_dict = self.get_accesed_document_group(user_pk, document_group_dict) 
+                if doc_user_group_dict:
+                    for doc_group_pk in doc_user_group_dict.keys():
+                        if doc_group_pk not in department_doc_groups[departmentfk].values():
+                            self.add_document_group_user(doc_group_pk, user_pk)
+                else:
+                    for pk in department_doc_groups[departmentfk].values():
+                        self.add_document_group_user(pk, user_pk)
+
+
